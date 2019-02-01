343b4201daf656c8ab8324c132a30e7af6571973
balakrishnana <balakrishnana@GCDTP039.GADAGET.COM>
2019-02-01 17:58:11 +0530

    second commit

--- Exception/src/com/krish/except/CustomEmtpyException.java
+++ Exception/src/com/krish/except/CustomEmtpyException.java
@@ -1,10 +1,10 @@
 package com.krish.except;
 
-public class CustomEmtpyException extends Exception{
-	private static final long serialVersionUID = -1480985619750212454L; 
+public class CustomEmtpyException extends Exception {
+	private static final long serialVersionUID = -1480985619750212454L;
 
-	CustomEmtpyException(){
-		
+	CustomEmtpyException(String s) {
+		super(s);
 	}
-	
+
 }
